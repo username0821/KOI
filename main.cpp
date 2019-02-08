73ac53605c7fa79b3c64c005be69fd4e9ed7e51d
username0821 <47444630+username0821@users.noreply.github.com>
2019-02-08 17:21:41 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main()
+{
+    int a, b, c, i, j;
+    scanf("%d", &a);
+    for(i = 1; i < a+1; i++)
+    {
+        if(a % 3)
+        {
+            printf("%d ", i);
+        }
+        else()
+        {
+            printf("X ");
+        }
+    }
+     return 0;
+}
